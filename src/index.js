d5511c89927184f867534f88ab9c10e13bf62e07
Daniel Laloush <d.laloush@outlook.com>
2020-04-16 18:12:08 -0400

    added tab functionalities in Index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import './style.scss';
 
+let pageSetup = null;
+let createImg = null;
+let containers = null;
+
+function menuNav() {
+  const name = this.innerHTML.toLowerCase();
+  createImg.current(name);
+  containers.currentMenu(name);
+}
+
+/** Create Main Page Setup with Quick Access */
 const PageSetup = () => {
   const structure = [];
 
   const header = document.createElement('header');
+  const img = document.createElement('div');
+  img.className = 'img-container';
   const main = document.createElement('main');
   const footer = document.createElement('footer');
 
+  structure.push(img);
   structure.push(header);
   structure.push(main);
   structure.push(footer);
@@ -17,9 +31,120 @@ const PageSetup = () => {
     mainContainer.appendChild(obj);
   });
 
-  return { header, main, footer };
+  return {
+    header, img, main, footer,
+  };
+};
+
+/** Create Main Left Img with Img Controls */
+const LeftImgSetUp = () => {
+  const div = document.createElement('div');
+  div.className = 'main-img';
+
+  const home = document.createElement('img');
+  home.src = '../src/img/home.jpg';
+  home.className = 'show';
+  home.name = 'home';
+
+  const contact = document.createElement('img');
+  contact.src = '../src/img/contact.jpg';
+  contact.className = 'noshow';
+  contact.name = 'contact';
+
+  const menu = document.createElement('img');
+  menu.src = '../src/img/menu.jpg';
+  menu.className = 'noshow';
+  menu.name = 'menu';
+
+  div.appendChild(home);
+  div.appendChild(contact);
+  div.appendChild(menu);
+  pageSetup.img.appendChild(div);
+
+  const current = (name) => {
+    const arrOfImg = div.querySelectorAll('img');
+    arrOfImg.forEach((img) => {
+      if (img.name === name) {
+        img.setAttribute('class', 'show');
+      } else {
+        img.setAttribute('class', 'noshow');
+      }
+    });
+  };
+
+  return { current };
+};
+
+/** Create Header Nav Section */
+const Header = () => {
+  const menuArr = ['Menu', 'Contact', 'Home'];
+  const div = document.createElement('div');
+  const menu = document.createElement('menu');
+  const ul = document.createElement('ul');
+  menuArr.forEach((element) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerHTML = element;
+    a.href = '#';
+    a.onclick = menuNav;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  menu.appendChild(ul);
+  div.appendChild(menu);
+
+  pageSetup.header.appendChild(div);
+
+  return { div, menuArr };
+};
+
+/** Create the Main Container */
+const MainContainer = () => {
+  const arrElements = [];
+  const home = document.createElement('div');
+  home.id = 'home';
+  arrElements.push(home);
+
+  const contact = document.createElement('div');
+  contact.id = 'contact';
+  arrElements.push(contact);
+
+  const menu = document.createElement('div');
+  menu.id = 'menu';
+  arrElements.push(menu);
+
+  const div = pageSetup.main;
+  div.appendChild(home);
+
+  const currentMenu = (name) => {
+    arrElements.forEach((element) => {
+      if (element.id === name) {
+        div.innerHTML = '';
+        div.appendChild(element);
+      }
+    });
+  };
+
+  return {
+    currentMenu, home, contact, menu,
+  };
+};
+
+
+/** Home Page */
+const HomePage = () => {
+  const h2 = document.createElement('h2');
+  h2.innerHTML = 'This is the Best Restaurant Page EVER ! ! !';
+  containers.home.appendChild(h2);
 };
 
 window.onload = () => {
-  const pageSetup = PageSetup(); /* eslint-disable-line */
+  pageSetup = PageSetup();
+  createImg = LeftImgSetUp();
+  // eslint-disable-next-line no-unused-vars
+  const header = Header();
+  containers = MainContainer();
+  // eslint-disable-next-line no-unused-vars
+  const homePage = HomePage();
 };
