998167cf1f2c40ff9951caf7eadfd21169182cb9
Daniel Laloush <d.laloush@outlook.com>
2020-04-16 13:46:16 -0400

    fixed eslint errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 const PageSetup = () => {
-  
   const structure = [];
 
-  const header = document.createElement('header')
-  const main = document.createElement('main')
-  const footer = document.createElement('footer')
+  const header = document.createElement('header');
+  const main = document.createElement('main');
+  const footer = document.createElement('footer');
 
-  structure.push(header)
-  structure.push(main)
-  structure.push(footer)
+  structure.push(header);
+  structure.push(main);
+  structure.push(footer);
 
-  const mainContainer = document.getElementById('content')
+  const mainContainer = document.getElementById('content');
 
-  structure.forEach(obj => {
-    mainContainer.appendChild(obj)
+  structure.forEach((obj) => {
+    mainContainer.appendChild(obj);
   });
 
-  return {header,main,footer}
-}
+  return { header, main, footer };
+};
 
 window.onload = () => {
-  const pageSetup = PageSetup();
-}
+  const pageSetup = PageSetup(); /* eslint-disable-line */
+};
