c4f20efdffbfd81157fd47a8f3630c4f0fe759be
Daniel Laloush <d.laloush@outlook.com>
2020-04-17 14:52:12 -0400

    added contact page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import * as menuList from './menu.json';
 let pageSetup = null;
 let createImg = null;
 let containers = null;
+let map = null;
 
+/** Tab Navigation */
 function menuNav() {
   const name = this.innerHTML.toLowerCase();
   createImg.current(name);
   containers.currentMenu(name);
 }
+
+/** Embed Google Maps */
+const GoogleMaps = () => {
+  const mapsContainer = document.createElement('div');
+  mapsContainer.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.681918696429!2d-82.05555198448155!3d28.609317691881042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e7fabb623b6279%3A0xf09495de73510819!2s99-1%20SE%201st%20St%2C%20Webster%2C%20FL%2033597%2C%20USA!5e0!3m2!1sen!2sve!4v1587148990175!5m2!1sen!2sve" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
+  return { mapsContainer };
+};
 
 /** Create Main Page Setup with Quick Access */
 const PageSetup = () => {
@@ -194,20 +203,29 @@ const MenuPage = () => {
 
 /** Contact Page */
 const ContactPage = () => {
+  const p1 = document.createElement('p');
+  p1.innerHTML = 'some contact info (AKA Email)';
+  const p2 = document.createElement('p');
+  p2.innerHTML = 'some phone';
+  const p3 = document.createElement('p');
+  p3.innerHTML = 'more phones';
+  const p4 = document.createElement('p');
+  p4.innerHTML = 'some instagram of facebook things';
 
+  containers.contact.appendChild(p1);
+  containers.contact.appendChild(p2);
+  containers.contact.appendChild(p3);
+  containers.contact.appendChild(p4);
+  containers.contact.appendChild(map.mapsContainer);
 };
 
 window.onload = () => {
   pageSetup = PageSetup();
   createImg = LeftImgSetUp();
-  // eslint-disable-next-line no-unused-vars
-  const header = Header();
   containers = MainContainer();
-  // eslint-disable-next-line no-unused-vars
-  const homePage = HomePage();
-  // eslint-disable-next-line no-unused-vars
-  const menuPage = MenuPage();
-  // eslint-disable-next-line no-unused-vars
-  const contactPage = ContactPage();
-  containers.currentMenu('menu');
+  map = GoogleMaps();
+  Header();
+  HomePage();
+  MenuPage();
+  ContactPage();
 };
